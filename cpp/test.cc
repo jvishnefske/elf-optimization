35c5c1018a4f154f71fa9047d96f7d8694b8638c
John Vishnefske <jvishnefske@gmail.com>
2014-12-31 21:46:31 -0800

    first two time tests pass.

--- cpp/test.cc
+++ cpp/test.cc
@@ -1,5 +1,9 @@
+
 #include <iostream>
 #include <ctime>
+#include "main.h"
+#include "queue.h"
+#include "time.h"
 #define  BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Elf
 //Boost Test Library Unit Test Framework [UTF].
@@ -7,32 +11,32 @@
 //http://www.boost.org/doc/libs/1_44_0/libs/test/doc/html/utf/testing-tools/output-test.html
 #include <boost/test/output_test_stream.hpp>
 #include <boost/test/unit_test.hpp>
-//#include <boost/test/included/prg_exec_monitor.hpp>//static library, calls cpp_main() 
-//#include <stdexcept>
-//extern int strToRel(const char*);
-//extern void relToStr(int, char*, int);
-extern int globalStart(void);
-#include "main.h"
-//BOOST_AUTO_TEST_CASE(FUNCTION);
-//BOOST_AUTO_TEST_SUITE(SUITE_NAME);
-//libunit_test_framework.lib should only be required to be linked 
-//if boost/test/unit_test.hpp is the only boost test include file. 
-//unit_test_log::instance().set_log_output( std::ostream& );
-//BOOST_TEST_LOG_LEVEL env variable
 
-//called from boost test libraries program execution monitor
-//int cpp_main(int argc, char *argv[]){
-
+/**
+ * @brief main unit testfunction.
+ * @details runs before the auto unit tests
+ * 
+ * @param argc argument count
+ * @param argv Command line arguments
+ * 
+ * @return shell returnvalue.
+ */
 int init_unit_test_suite(int argc, char *argv[]){
     //std::cout<<"running tests. " << std::endl;
     if(false){std::cout<<argv[argc-1];}
+
     return 0;
 }
+
+//BOOST_AUTO_TEST_CASE(test_name)
 BOOST_AUTO_TEST_CASE(globalStartTime){
+    Chronos &chronos = Chronos::getChronos();
     char *time;
-    time_t _globalStart = (time_t) globalStart();
+    const time_t _globalStart = (time_t) chronos.getGlobalStart();
     //also consider asctime(gmtime());
+
     time = ctime(&_globalStart);
+
     BOOST_CHECK_EQUAL(_globalStart, 1388563200);
     boost::test_tools::output_test_stream output;
     output<<time;
@@ -40,8 +44,9 @@ BOOST_AUTO_TEST_CASE(globalStartTime){
     BOOST_CHECK(output.is_equal("Wed Jan  1 00:00:00 2014\n"));
 }   
 BOOST_AUTO_TEST_CASE(conversion){
-    time_t _globalStart = (time_t) globalStart();
-    time_t relTime = strToRel("2014 1 1 0 0");
+    Chronos &chronos = Chronos::getChronos();
+    time_t _globalStart = (time_t) chronos.getGlobalStart();
+    time_t relTime = chronos.convertToMinutes("2014 1 1 0 0");
     time_t sumTime = _globalStart + relTime;
     //std::cout << relTime <<", " <<_globalStart << " converts to: " << ctime(&relTime) << std::endl;
     //std::cout<<relTime<<std::endl;
@@ -50,12 +55,44 @@ BOOST_AUTO_TEST_CASE(conversion){
 }
 
 BOOST_AUTO_TEST_CASE(conversion_to_string){
-    time_t relTime = strToRel("2014 1 1 0 0");
-    char stime[255];
-    relToStr(relTime,stime,254);
+    Chronos &chronos = Chronos::getChronos();
+    int relTime = chronos.convertToMinutes("2014 1 1 0 0");
+    //char stime[255];
+    //relToStr(relTime*60,stime,254);
+    std::string stime = 
+        chronos.toString(relTime);
+    chronos.toString(relTime);
     
     boost::test_tools::output_test_stream output;
     output << stime;
     BOOST_CHECK(output.is_equal("2014 1 1 0 0"));
 }
+BOOST_AUTO_TEST_CASE(update_state){
 
+    ElfState state1(0), state2(0);
+    Toy toy;
+    state1.efficiency = 2;
+    state1.nextStartTime = 9*60; // 0900 on first day.
+    toy.duration = 120;
+    updateState(state1,toy,state2);
+    BOOST_CHECK_CLOSE(state2.efficiency,2.0808,.00001);
+    BOOST_CHECK_EQUAL(state2.nextStartTime, 10*60);
+
+    state1.efficiency = .5;
+    state1.nextStartTime = 19*60-1; //18:59 day 1
+    toy.duration = 60*4;
+    updateState(state1,toy,state2);
+    BOOST_CHECK_CLOSE(state2.efficiency, 0.32805, .00001); // .5 * .9**4
+    BOOST_CHECK_EQUAL(state2.nextStartTime, 2460);  // 8 hours penalty on day2
+
+
+    //multiday durration toy
+    state1.efficiency = 4;
+    state1.nextStartTime =  1981; //9:01 day 2
+    toy.duration = 1440*8;
+    updateState(state1,toy,state2);
+    //next efficency = 4*.9**(14*2)*1.02**(20.)
+    BOOST_CHECK_CLOSE(state2.efficiency, 0.3110668209953771, .00001); 
+    BOOST_CHECK_EQUAL(state2.nextStartTime, 4861);  // 8 hours penalty on day2
+
+}
--- cpp/time.h
+++ cpp/time.h
@@ -1,6 +1,138 @@
+
+#ifndef TIME_H
+#define TIME_H
+#define DIV_CEIL(x,y) (int)(((x+y)/(float)(y))  -.0000001 )
 #include <ctime>
-//const char* timeFormat = "%Y %-m %-d %-H %-M";
-extern time_t globalStart();
-extern time_t strToRel(const char*);
-extern char* relToStr(int ,char*,int );
+#include <string>
+#include <cassert>
+
+class Chronos 
+{
+  // we create this as a singleton class since some of the elements
+  // are created with the
+ private:
+   //static Chronos *chronos;
+  Chronos() {
+    static struct std::tm tm_globalStart;
+    strptime("2014 1 1 0 0", timeFormat, &tm_globalStart);
+    globalStart = mktime(&tm_globalStart);
+  }
+  Chronos(Chronos const&);
+  void operator=(Chronos const&);
+
+  const char *timeFormat = "%Y %-m %-d %-H %-M";
+  const int hoursPerDay = 10;
+  const int minutesPerWorkday = hoursPerDay * 60;
+  const int unsanctionedHoursPerDay = 14;
+  const int dayStart = 9 * 60;
+  const int dayEnd = 19 * 60;
+  const int minutesIn24h = 1440;
+  time_t globalStart; 
+ public:
+ 	time_t getGlobalStart()
+ 		{return globalStart;}
+  static Chronos &getChronos(){
+  	static Chronos instance;
+    return instance;
+  }
+  int convertToMinutes(const std::string &date) {
+    /** Input: time format
+     * output: 
+     */
+    
+    // strptime returns invalid  time_t when struct tm not static.
+    static struct std::tm tm_date;
+    strptime(date.c_str(), timeFormat, &tm_date);
+    time_t t_date = mktime(&tm_date);
+
+    double myTime = difftime(t_date, globalStart);
+    return (int) myTime/60;
+
+  }
+
+   std::string toString(int minutes) {
+    char tempChar[128];
+    size_t maxLen = 127;
+    //int relTime = minutes*60;
+    
+    time_t time = globalStart + (time_t) minutes*60;
+    struct std::tm tm_time;
+    localtime_r(&time, &tm_time);
+  
+    strftime(tempChar, maxLen, timeFormat, &tm_time);
+    
+    return std::string(tempChar);
+  }
+
+  bool inline isSanctonedTime(int minute) {
+ 
+  	int dayTime = minute %minutesIn24h;
+    return (dayTime >= dayStart && dayTime < dayEnd) ? true: false;
+  }
+/**
+ * @brief maps toy duration to unsanctioned, and sanctioned blocks
+ * @details take out whole days (24-hour periods contribute fixed )
+ * 			then use basic substution to calculate remaining time.
+ * @param minute elapsed minutes since globalStartTime
+ * @param  sanctionedHours in terms of toy durration.
+ * @param  unsanctionedHours also in terms of toy durration.
+ */
+  void getSanctionedBreakDown(
+  		int startMinute,
+   		int toyDuration,
+   		float efficency,
+   		float &sanctionedHours,
+   		float &unsanctionedHours) // in toy duration
+  {
+  	int minutes = DIV_CEIL(toyDuration,efficency);
+  	int endMinute = startMinute + minutes;
+	int fullDays = minutes / minutesIn24h;
+	if (!isSanctonedTime(startMinute))
+		std::cout<< "the offending time is: "<<startMinute<<std::endl;
+	assert(isSanctonedTime(startMinute));
+	if(isSanctonedTime(endMinute-1)){ //inclusive
+		//the only unsanctioned time is full days.
+		//since we toyDurration=sanctioned+unsanctioned we can solve.
+		unsanctionedHours = fullDays * unsanctionedHoursPerDay * efficency * hoursPerDay;
+		sanctionedHours = toyDuration / 60-unsanctionedHours;
+	}else{
+		const int endOfDayMinute = 19*60;
+		sanctionedHours = fullDays * hoursPerDay + 
+		(endOfDayMinute-startMinute)/60*efficency;
+		unsanctionedHours = toyDuration / 60 -sanctionedHours;
+	}
+  }
+  /**
+   * @brief next sanctioned minute including penalty
+   * @details takes a minute, and returns the next time which is within 
+   * 	a 9:00-19:00 window.
+   */
+  int nextSanctionedTime(int minute){
+  	int sanctionedStart;
+  	if(isSanctonedTime(minute)){
+  		return minute;
+  	}else{
+  		const int day=(minute+14*60)/minutesIn24h;
+  		sanctionedStart = day * minutesIn24h + dayStart;
+  	}
+  	return sanctionedStart;
+  }
+  int nextStartTime(
+  		int startMinute,
+  		float efficency,
+  		float unsanctionedHours,
+  		int &nextStartTime)
+  {
+  	//penalty is only added to sanctioned hours
+  	
+  	//now calculate penalty, and add.
+  	nextStartTime = nextSanctionedTime(startMinute);
+  	int penalty = DIV_CEIL(unsanctionedHours*60, efficency);
+  	int penaltyMinutes = penalty % minutesPerWorkday;
+  	int penaltyDays = penalty / minutesPerWorkday;
+  	nextStartTime += penaltyDays * minutesIn24h + penaltyMinutes;
+  	return nextStartTime;
+  }
+};
+#endif // TIME_H
 
